d61038fec8473177dcc441b5b296681f8f6b2c63
Anthony <bskhacker@gmail.com>
2020-03-23 16:03:42 -0400

    First version of Todo list from React crash course.

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>This is the TodoList app v1.0.0. It is part of a React crash course</h1>
+        </React.Fragment>
+    )
+}
+
+
+export default About
