59fc2a4e38459d16f6b0a60d4d5d96f4b662ae05
Anthony <bskhacker@gmail.com>
2020-03-23 21:21:59 -0400

    Updates

---
+++ precache-manifest.d76c41db3bb2f518797f924cbdff089b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc072002143af930fe9801fd56e3dcf",
+    "url": "/react_crashcourse_todo/index.html"
+  },
+  {
+    "revision": "b669e105281ad3b00df5",
+    "url": "/react_crashcourse_todo/static/css/main.54c48da3.chunk.css"
+  },
+  {
+    "revision": "389fc70058546698bcd7",
+    "url": "/react_crashcourse_todo/static/js/2.3c2e5f0a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react_crashcourse_todo/static/js/2.3c2e5f0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b669e105281ad3b00df5",
+    "url": "/react_crashcourse_todo/static/js/main.5d919c97.chunk.js"
+  },
+  {
+    "revision": "0bebac5043f9aaf772a7",
+    "url": "/react_crashcourse_todo/static/js/runtime-main.52c15be7.js"
+  }
+]);
